8f169201d1dec823c52f97f61e63007ddddb90c4
YXT-yang <1453231310@qq.com>
2025-10-27 10:13:02 +0800

    Update folders

---
+++ tool/spline_main.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file spline_main.cpp
+ * @author Xiaoteng Yang (xtyang@whu.edu.cn)
+ * @brief Xiaoteng Yang received the B.S. degree in 
+ * geodesy and geomatics engineering from Wuhan University, Wuhan China, in 2023. 
+ * He is currently a master candidate at the school of Geodesy and Geomatics, Wuhan University.
+ * @version 0.1
+ * @date 2025-10-27
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
